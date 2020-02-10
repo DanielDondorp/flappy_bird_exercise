7678ca2c9326de2751ca9888b4f9dfb4077d2415
daniel <danieldondorp@gmail.com>
2020-02-10 15:15:55 +0100

    fixed highscore bug

--- flappy_bird_exercise/game.py
+++ flappy_bird_exercise/game.py
@@ -35,7 +35,7 @@ class Game:
     def make_world(self):
         self.bird = Bird(pos = self.height//2, size = 10)
         self.obstacles = [Barrier(pos = self.width)]
-        self.high_score = self.score
+        self.high_score = np.max([self.score, self.high_score])
         self.score = 0
         self.game_number += 1
         
